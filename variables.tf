83aaaba25e39c7fd06128ae1f7878684f71a7563
Duru <durumanohar.tx@gmail.com>
2020-09-18 19:45:28 -0700

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,21 @@
+
+variable "location" {
+  default = "East US"
+}
+
+
+
+variable "subscription_id" {
+  default = "XXXXXXXXX-eb0a-438f-9866-xxxxxxxxx"
+}
+
+variable "client_id" {
+  default = "e04771b5-XXXXXXX-4cb7-XXXXXX-xxxxx"
+}
+
+variable "client_secret" {
+  default = "XXXXXXXXXX"
+}
+variable "tenant_id" {
+  default = "3xxxxxxxxxxx"
+}
